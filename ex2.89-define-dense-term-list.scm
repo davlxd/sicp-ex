b0cf776138ed56902dd0b9d8aeb674718c9ab2b3
lxd <i@lxd.me>
2019-03-06 20:42:01 +1300

    ex2.89, and ex2.90 WIP

---
+++ ex2.89-define-dense-term-list.scm
@@ -0,0 +1,14 @@
+
+
+(define (adjoin-term term term-list) (cons term term-list))
+(define (the-empty-termlist) '())
+(define (first-term term-list) (make-term (length term-list) (car term-list)))
+(define (rest-terms term-list) (cdr term-list))
+(define (empty-termlist? term-list) (null? term-list))
+(define (make-term order coeff) (list order coeff))
+(define (order term) (car term))
+(define (coeff term) (cadr term))
+
+
+
+(first-term (adjoin-term 'ex (adjoin-term 0 (adjoin-term 'asdf (the-empty-termlist)))))
