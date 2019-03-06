3bf67ff7b4ad3c1b3a3d48d2106c7009e2dcaa99
lxd <i@lxd.me>
2019-03-06 19:04:15 +1300

    Revisit, imp get/put and fix things

--- ex2.73-data-directed-deriv.scm
+++ ex2.73-data-directed-deriv.scm
@@ -1,28 +1,28 @@
-(define (deriv exp var)
-  (display "deriv ... ")
+(define (deriv0 exp var)
+  (display "deriv0 ... ")
   (write-line exp)
   (cond ((number? exp) 0)
 	((variable? exp)
 	 (if (same-variable? exp var) 1 0))
 	((sum? exp)
 	 (write-line "deconstruct sum")
-	 (make-sum (deriv (addend exp) var)
-		   (deriv (augend exp) var)))
+	 (make-sum (deriv0 (addend exp) var)
+		   (deriv0 (augend exp) var)))
 	((product? exp)
 	 (write-line "deconstruct product")
 	 (make-sum
 	   (make-product (multiplier exp)
-			 (deriv (multiplicand exp) var))
-	   (make-product (deriv (multiplier exp) var)
+			 (deriv0 (multiplicand exp) var))
+	   (make-product (deriv0 (multiplier exp) var)
 			 (multiplicand exp))))
 	((exponentiation? exp)
 	 (write-line "deconstruct exponentiation")
 	 (make-product
 	   (make-product (exponent exp)
 			 (make-exponentiation (base exp) (- (exponent exp) 1)))
-	   (deriv (base exp) var)))
+	   (deriv0 (base exp) var)))
 	(else
-	  (error "unknown expression type -- DERIV" exp))))
+	  (error "unknown expression type -- DERIV0" exp))))
 
 
 (define (variable? x) (symbol? x))
@@ -31,12 +31,7 @@
 
 (define (sum? x) (and (pair? x) (eq? (car x) '+)))
 (define (addend s) (cadr s))
-(define (augend s) 
-  (if (= (length (cddr s)) 1)
-    (caddr s)
-    (cons '+ (cddr s))))
-; this is also cool:
-; (define (augend s) (accumulate make-sum 0 (cddr s)))
+(define (augend s) (reduce make-sum 0 (cddr s)))
 
 (define (make-sum a1 a2)
   (cond ((=number? a1 0) a2)
@@ -65,7 +60,7 @@
 (define base cadr)
 (define exponent caddr)
 
-(deriv '(* x y (+ x 3)) 'x)
+(deriv0 '(* x y (+ x 3)) 'x)
 
 
 
@@ -83,6 +78,24 @@
 (define (same-variable? v1 v2) (and (variable? v1) (variable? v2) (eq? v1 v2)))
 (define (=number? exp num) (and (number? exp) (= exp num)))
 ; lib end
+
+
+; implmenet get and put
+(define table '())
+(define (get . args)
+  (let ((target-list (find (lambda (entry) (equal? args (sublist entry 0 (length args)))) table)))
+    (if target-list
+      (list-ref target-list (length args))
+      #f)))
+(define (put . args) (set! table (cons args table)))
+; (put 'hello 'world 3 3)
+; (put 'hello 'word 1 2)
+; (write-line table)
+; (get 'hello)
+; (get 'hello 'world)
+; (get 'hello 'word)
+; (get 'hello 'word 3)
+; (get 'hello 'word 1)
 
 
 (define (deriv exp var)
@@ -110,7 +123,7 @@
 
   (define (handle-sum operands var)
     (define addend car)
-    (define (augend s) (accumulate make-sum 0 (cdr s)))
+    (define (augend s) (reduce make-sum 0 (cdr s)))
     (make-sum (deriv (addend operands) var)
 	      (deriv (augend operands) var)))
 
@@ -130,8 +143,38 @@
   (put 'deriv '* handle-product)
   'done)
 
+(install-deriv)
+(deriv0 '(* x y (+ x 3)) 'x)
+(deriv '(* x y (+ x 3)) 'x)
 
 ; c.
+
+
+; lib
+(define (variable? x) (symbol? x))
+(define (same-variable? v1 v2) (and (variable? v1) (variable? v2) (eq? v1 v2)))
+(define (=number? exp num) (and (number? exp) (= exp num)))
+; lib end
+
+
+; implmenet get and put
+(define table '())
+(define (get . args)
+  (let ((target-list (find (lambda (entry) (equal? args (sublist entry 0 (length args)))) table)))
+    (if target-list
+      (list-ref target-list (length args))
+      #f)))
+(define (put . args) (set! table (cons args table)))
+;
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+	((variable? exp) (if (same-variable? exp var) 1 0))
+	(else ((get 'deriv (operator exp)) (operands exp)
+					   var))))
+(define (operator exp) (car exp))
+(define (operands exp) (cdr exp))
+
 
 (define (install-makers)
   (define (make-sum a1 a2)
@@ -150,21 +193,28 @@
   (put 'maker 'sum make-sum)
   (put 'maker 'product make-product)
   'done)
-
+(install-makers)
 
 
 (define (install-deriv-sum)
+  (define (make-sum a1 a2)
+    (cond ((=number? a1 0) a2)
+	  ((=number? a2 0) a1)
+	  ((and (number? a1) (number? a2)) (+ a1 a2))
+	  (else (list '+ a1 a2))))
+
   (define (handle-sum operands var)
     (define addend car)
-    (define (augend s) (accumulate make-sum 0 (cdr s)))
+    (define (augend s) (reduce make-sum 0 (cdr s)))
     ((get 'maker 'sum) (deriv (addend operands) var)
 		       (deriv (augend operands) var)))
 
   (put 'deriv '+ handle-sum)
   'done)
+(install-deriv-sum)
 
 
-(define (install-deriv-sum)
+(define (install-deriv-product)
   (define (handle-product operands var)
     (define (multiplier p) (car p))
     (define (multiplicand p) 
@@ -179,6 +229,8 @@
 
   (put 'deriv '* handle-product)
   'done)
+(install-deriv-product)
+
 
 (define (install-maker-exponentiation)
   (define (make-exponentiation base exponent)
@@ -188,7 +240,7 @@
 
   (put 'maker 'expoentiation make-exponentiation)
   'done)
-
+(install-maker-exponentiation)
 
 
 (define (install-deriv-exponentiation)
@@ -201,10 +253,11 @@
 			    ((get 'maker 'expoentiation) (base operands) (- (exponent operands) 1)))
      (deriv (base operands) var)))
 
-  (put 'deriv '** handle-product)
+  (put 'deriv '** handle-exponentiation)
   'done)
+(install-deriv-exponentiation)
 
-
+(deriv '(* x y (+ x 3)) 'x)
 
 ; d.
 
--- ex2.80-zero.scm
+++ ex2.80-zero.scm
@@ -1,3 +1,184 @@
+;; TAG LIB
+(define (attach-tag type-tag contents)
+  (cons type-tag contents))
+(define (type-tag datum)
+  (if (pair? datum)
+    (car datum)
+    (error "Bad tagged datum -- TYPE-TAG" datum)))
+(define (contents datum)
+  (if (pair? datum)
+    (cdr datum)
+    (error "Bad tagged datum -- CONTENTS" datum)))
+;;
+
+;; PUT/GET
+(define table '())
+(define (get . args)
+  (let ((target-list (find (lambda (entry) (equal? args (sublist entry 0 (length args)))) table)))
+    (if target-list
+      (list-ref target-list (length args))
+      #f)))
+(define (put . args) (set! table (cons args table)))
+;;
+
+
+;; INSTALL-SCHEME-NUMBER
+(define (install-scheme-number-package)
+  (define (tag x)
+    (attach-tag 'scheme-number x))
+  (put 'add '(scheme-number scheme-number)
+       (lambda (x y) (tag (+ x y))))
+  (put 'sub '(scheme-number scheme-number)
+       (lambda (x y) (tag (- x y))))
+  (put 'mul '(scheme-number scheme-number)
+       (lambda (x y) (tag (* x y))))
+  (put 'div '(scheme-number scheme-number)
+       (lambda (x y) (tag (/ x y))))
+  (put 'make 'scheme-number
+       (lambda (x) (tag x)))
+  'done)
+(install-scheme-number-package)
+(define (make-scheme-number n) ((get 'make 'scheme-number) n))
+;;
+
+;; INSTALL RATIONAL
+(define (numer x) (car x))
+(define (denom x) (cdr x))
+(define (install-rational-package)
+  ;; internal procedures
+  (define (make-rat n d)
+    (let ((g (gcd n d)))
+      (cons (/ n g) (/ d g))))
+  (define (add-rat x y)
+    (make-rat (+ (* (numer x) (denom y))
+		 (* (numer y) (denom x)))
+	      (* (denom x) (denom y))))
+  (define (sub-rat x y)
+    (make-rat (- (* (numer x) (denom y))
+		 (* (numer y) (denom x)))
+	      (* (denom x) (denom y))))
+  (define (mul-rat x y)
+    (make-rat (* (numer x) (numer y))
+	      (* (denom x) (denom y))))
+  (define (div-rat x y)
+    (make-rat (* (numer x) (denom y))
+	      (* (denom x) (numer y))))
+  ;; interface to rest of the system
+  (define (tag x) (attach-tag 'rational x))
+  (put 'add '(rational rational)
+       (lambda (x y) (tag (add-rat x y))))
+  (put 'sub '(rational rational)
+       (lambda (x y) (tag (sub-rat x y))))
+  (put 'mul '(rational rational)
+       (lambda (x y) (tag (mul-rat x y))))
+  (put 'div '(rational rational)
+       (lambda (x y) (tag (div-rat x y))))
+
+  (put 'make 'rational
+       (lambda (n d) (tag (make-rat n d))))
+  'done)
+(install-rational-package)
+(define (make-rational n d) ((get 'make 'rational) n d))
+;;
+
+
+;; INSTALL COMPLEX
+
+(define (install-rectangular-package)
+  ;; internal procedures
+  (define (real-part z) (car z))
+  (define (imag-part z) (cdr z))
+  (define (make-from-real-imag x y) (cons x y))
+  (define (magnitude z)
+    (sqrt (+ (square (real-part z))
+	     (square (imag-part z)))))
+  (define (angle z)
+    (atan (imag-part z) (real-part z)))
+  (define (make-from-mag-ang r a) 
+    (cons (* r (cos a)) (* r (sin a))))
+  ;; interface to the rest of the system
+  (define (tag x) (attach-tag 'rectangular x))
+  (put 'real-part '(rectangular) real-part)
+  (put 'imag-part '(rectangular) imag-part)
+  (put 'magnitude '(rectangular) magnitude)
+  (put 'angle '(rectangular) angle)
+  (put 'make-from-real-imag 'rectangular 
+       (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'rectangular 
+       (lambda (r a) (tag (make-from-mag-ang r a))))
+  'done)
+(install-rectangular-package)
+
+(define (install-polar-package)
+  ;; internal procedures
+  (define (magnitude z) (car z))
+  (define (angle z) (cdr z))
+  (define (make-from-mag-ang r a) (cons r a))
+  (define (real-part z)
+    (* (magnitude z) (cos (angle z))))
+  (define (imag-part z)
+    (* (magnitude z) (sin (angle z))))
+  (define (make-from-real-imag x y) 
+    (cons (sqrt (+ (square x) (square y)))
+	  (atan y x)))
+  ;; interface to the rest of the system
+  (define (tag x) (attach-tag 'polar x))
+  (put 'real-part '(polar) real-part)
+  (put 'imag-part '(polar) imag-part)
+  (put 'magnitude '(polar) magnitude)
+  (put 'angle '(polar) angle)
+  (put 'make-from-real-imag 'polar
+       (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'polar 
+       (lambda (r a) (tag (make-from-mag-ang r a))))
+  'done)
+(install-polar-package)
+(define (real-part z) (apply-generic 'real-part z))
+(define (imag-part z) (apply-generic 'imag-part z))
+(define (magnitude z) (apply-generic 'magnitude z))
+(define (angle z) (apply-generic 'angle z))
+
+(define (install-complex-package)
+  ;; imported procedures from rectangular and polar packages
+  (define (make-from-real-imag x y)
+    ((get 'make-from-real-imag 'rectangular) x y))
+  (define (make-from-mag-ang r a)
+    ((get 'make-from-mag-ang 'polar) r a))
+  ;; internal procedures
+  (define (add-complex z1 z2)
+    (make-from-real-imag (+ (real-part z1) (real-part z2))
+			 (+ (imag-part z1) (imag-part z2))))
+  (define (sub-complex z1 z2)
+    (make-from-real-imag (- (real-part z1) (real-part z2))
+			 (- (imag-part z1) (imag-part z2))))
+  (define (mul-complex z1 z2)
+    (make-from-mag-ang (* (magnitude z1) (magnitude z2))
+		       (+ (angle z1) (angle z2))))
+  (define (div-complex z1 z2)
+    (make-from-mag-ang (/ (magnitude z1) (magnitude z2))
+		       (- (angle z1) (angle z2))))
+  ;; interface to rest of the system
+  (define (tag z) (attach-tag 'complex z))
+  (put 'add '(complex complex)
+       (lambda (z1 z2) (tag (add-complex z1 z2))))
+  (put 'sub '(complex complex)
+       (lambda (z1 z2) (tag (sub-complex z1 z2))))
+  (put 'mul '(complex complex)
+       (lambda (z1 z2) (tag (mul-complex z1 z2))))
+  (put 'div '(complex complex)
+       (lambda (z1 z2) (tag (div-complex z1 z2))))
+  (put 'make-from-real-imag 'complex
+       (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'complex
+       (lambda (r a) (tag (make-from-mag-ang r a))))
+  'done)
+(install-complex-package)
+(define (make-complex-from-real-imag x y) ((get 'make-from-real-imag 'complex) x y))
+(define (make-complex-from-mag-ang r a) ((get 'make-from-mag-ang 'complex) r a))
+;;
+
+
+
 (define (apply-generic op . args)
   (let ((type-tags (map type-tag args)))
     (let ((proc (get op type-tags)))
@@ -14,3 +195,25 @@
   (put '=zero? '(rational) (lambda (x) (= (numer x) 0)))
   (put '=zero? '(complex) (lambda (x) (and (= 0 (real-part x)) (= 0 (imag-part x)))))
   'done)
+(install-=zero?)
+
+(=zero? (make-scheme-number 10))
+(=zero? (make-scheme-number 0))
+
+(=zero? (make-rational 3 1))
+(=zero? (make-rational 0 1))
+(=zero? (make-rational 0 4))
+
+(=zero? (make-complex-from-real-imag 1 2))
+(=zero? (make-complex-from-real-imag 0 2))
+(=zero? (make-complex-from-real-imag 0 0))
+
+(=zero? (make-complex-from-mag-ang 0 0))
+(=zero? (make-complex-from-mag-ang 0 2))
+(=zero? (make-complex-from-mag-ang 0 1))
+
+
+
+
+
+
