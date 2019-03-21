fcf3b6f2ac332a7b5f1b7c25a3680ac16ae72b73
lxd <i@lxd.me>
2019-03-21 19:00:25 +1300

    Rectify

--- ex3.6-reset-rand.scm
+++ ex3.6-reset-rand.scm
@@ -53,3 +53,84 @@
 (rand1 'generate)
 
 
+
+
+;; Correction: Actually there is another better way:
+
+(define rand2
+  (let ((x random-init))
+    (define x random-init)
+    (define (generate)
+      (begin
+        (set! x (rand-update x))
+        x))
+    (define (reset y) (set! x y))
+
+    (lambda (arg)
+      (cond ((eq? arg 'generate) (generate))
+            ((eq? arg 'reset) reset)))
+    )
+  )
+
+(rand2 'generate)
+(rand2 'generate)
+((rand2 'reset) 1)
+(rand2 'generate)
+(rand2 'generate)
+
+; Actually the return of lambda which that access another variable natually gives us closure
+; unfortunately MIT-scheme doesn't allow this form
+; (define rand2 (define x random-init) (define ... ...) (define ... ...) ...)
+;
+;
+; Another thing to notice is rand2 always gives us the same lambda, because it was DEFINED
+rand2
+;Value 13: #[compound-procedure 13]
+rand2
+;Value 13: #[compound-procedure 13]
+;
+;
+; So is rand:
+rand
+;Value 14: #[compound-procedure 14 rand]
+rand
+;Value 14: #[compound-procedure 14 rand]
+;
+;
+; However (rand), an execution of a function, creates a new lambda each time:
+(rand)
+;Value 15: #[compound-procedure 15]
+(rand)
+;Value 16: #[compound-procedure 16]
+(rand)
+;Value 17: #[compound-procedure 17]
+
+
+;
+;
+; Let's go wild:
+;
+(define rand3
+  (
+   (lambda () 
+     (define x random-init)
+     (define (generate)
+       (begin
+         (set! x (rand-update x))
+         x))
+     (define (reset y) (set! x y))
+
+     (lambda (arg)
+       (cond ((eq? arg 'generate) (generate))
+             ((eq? arg 'reset) reset)))
+
+     )
+   )
+  )
+
+(rand3 'generate)
+(rand3 'generate)
+((rand3 'reset) 1)
+(rand3 'generate)
+(rand3 'generate)
+
