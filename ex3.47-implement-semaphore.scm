16add09dc4856178a5c920bb5556d56096d63aa4
lxd <i@lxd.me>
2019-04-02 20:49:23 +1300

    Rectify

--- ex3.47-implement-semaphore.scm
+++ ex3.47-implement-semaphore.scm
@@ -66,6 +66,22 @@
 ;
 ;
 
+; Reliable answer from schemewiki:
+(define (make-semaphore n) 
+  (let ((lock (make-mutex)) 
+        (taken 0)) 
+    (define (semaphore command) 
+      (cond ((eq? command 'acquire) 
+             (lock 'acquire) 
+             (if (< taken n) 
+               (begin (set! taken (1+ taken)) (lock 'release)) 
+               (begin (lock 'release) (semaphore 'acquire)))) 
+            ((eq? command 'release) 
+             (lock 'acquire) 
+             (set! taken (1- taken)) 
+             (lock 'release)))) 
+    semaphore)) 
+
 
 
 
