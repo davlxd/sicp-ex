d061629b32b793f4cd91c3ec12a036386d120927
lxd <i@lxd.me>
2019-04-08 19:07:05 +1200

    ex3.64

---
+++ ex3.64-stream-limit-with0-tolerance.scm
@@ -0,0 +1,40 @@
+(define (display-line x) (newline) (display x))
+(define (display-stream s n)
+  (define (stream-for-each proc s n)
+    (if (or (stream-null? s) (= n 0))
+      'done
+      (begin (proc (stream-car s))
+             (stream-for-each proc (stream-cdr s) (- n 1)))))
+  (stream-for-each display-line s n))
+
+(define (average a b) (/ (+ a b) 2.0))
+(define (sqrt-improve guess x)
+    (average guess (/ x guess)))
+
+(define (sqrt-stream x)
+  (define guesses
+    (cons-stream 1.0
+                 (stream-map (lambda (guess)
+                               (sqrt-improve guess x))
+                             guesses)))
+  guesses)
+
+
+(display-stream (sqrt-stream 2.0) 10)
+
+
+(define (sqrt x tolerance)
+    (stream-limit (sqrt-stream x) tolerance))
+
+
+(define (stream-limit s tolerance)
+  (define (iter a b s)
+    (if (<= (abs (- a b)) tolerance)
+      b
+      (iter b (stream-car s) (stream-cdr s))))
+  (iter (stream-car s) (stream-car (stream-cdr s)) (stream-cdr (stream-cdr s))))
+
+(sqrt 2 1.0)
+(sqrt 2 0.1)
+(sqrt 2 0.000001)
+
