e73315b8a88a527b8da229c30a32791cd0d1e3e7
lxd <i@lxd.me>
2019-04-20 14:08:00 +1200

    ex4.15

---
+++ ex4.15-halt-paradox.scm
@@ -0,0 +1,8 @@
+; A paradox
+;
+; Based on definition of (try p)
+; if (try try) halts, it runs forever, if it runs forever, it halts
+;
+; So we never know wheter (try try) halts or run forever
+;
+
